ed5d5d6e6e2b3d2bb90be79b94f5496e06686134
Artemy <148975203+ArtMart1@users.noreply.github.com>
2024-11-05 19:45:03 +0300

    bug fix

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -5,9 +5,14 @@ class UserController {
     try {
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
-      res.cookie('refreshToken', userData.refreshToken, {});
+      res.cookie('refreshToken', userData.refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+      });
       return res.json(userData);
-    } catch (e) {}
+    } catch (e) {
+      console.log(e);
+    }
   }
   async login(req, res, next) {
     try {
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,23 +1,26 @@
-const UserModele = require('../modeles/user-modele');
+const UserModel = require('../modeles/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
-const mailServise = require('./mail-service');
+const mailServiсe = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
 
 class UserService {
   async registration(email, password) {
-    const candidate = await UserModele.findOne({ email });
+    const candidate = await UserModel.findOne({ email });
     if (candidate) {
-      throw new Error(`Пользователь с такоим ${email} уже существует`);
+      throw new Error(`Пользователь с таким ${email} уже существует`);
     }
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4();
-    const user = await UserModele.create({ email, password: hashPassword, activationLink });
+    const user = await UserModel.create({ email, password: hashPassword, activationLink });
     const userDto = new UserDto(user);
-    await mailServise.sendActivationmail(email, activationLink);
+    await mailServiсe.sendActivationmail(
+      email,
+      `${process.env.API_URL}/api/activate/activationLink`,
+    );
     const tokens = tokenService.generateTokens({ ...userDto });
-    await tokenService.saveToken(userDto, id, tokens.refreshToken);
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
     return {
       ...tokens,
       user: userDto,
